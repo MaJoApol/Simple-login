a0a4f397d5e5744eb21d91ee4a044899fb8468f0
MaJoApol <mariajoanaceron@gmail.com>
2024-12-06 17:44:37 -0300

    update git ignore

---
+++ Simple-login/src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeP2pk5KzKUsxtLJkiFB8hYgnZujk7xSA",
+  authDomain: "simple-login-d5fb5.firebaseapp.com",
+  projectId: "simple-login-d5fb5",
+  storageBucket: "simple-login-d5fb5.firebasestorage.app",
+  messagingSenderId: "953860095166",
+  appId: "1:953860095166:web:52314525de6bf01914f2ef"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
